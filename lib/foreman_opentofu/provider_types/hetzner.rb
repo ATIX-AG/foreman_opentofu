30e3362590559aeba72541edf21b92fdfca6e6f0
Markus Bucher <bucher@atix.de>
2026-01-28 18:42:37 +0100

    feat(provider): Add Hetzner provider

---
+++ lib/foreman_opentofu/provider_types/hetzner.rb
@@ -0,0 +1,58 @@
+# FIXME: should not be necessary due to autoloading :-(
+require "#{ForemanOpentofu::Engine.root}/app/services/foreman_opentofu/provider_type_manager"
+require "#{ForemanOpentofu::Engine.root}/app/services/foreman_opentofu/provider_type"
+
+ForemanOpentofu::ProviderTypeManager.register('hetzner') do
+  @capabilities = [:image]
+
+  def provided_attributes
+    {
+      ip: :vm_ip_address,
+      ip6: :vm_ip6_address,
+    }
+  end
+
+  def vm_ready(_vm)
+    # always ready ;-)
+    true
+  end
+
+  self.provider_attrs = [
+    { name: 'server_type', type: 'select', group: 'vm', mandatory: true,
+      label: 'Server Type', options: {
+        data_source: {
+          name: 'hcloud_server_types',
+          arguments: {},
+        },
+        entity: {
+          id: 'name',
+          name: 'description',
+        },
+        output_path_postfix: 'server_types',
+      } },
+    { name: 'location', type: 'select', group: 'vm',
+      options: {
+        data_source: {
+          name: 'hcloud_locations',
+          arguments: {},
+        },
+        entity: {
+          id: 'name',
+          name: 'description',
+        },
+        output_path_postfix: 'locations',
+      } },
+    { name: 'backups', type: 'bool', group: 'vm', help: 'Whether backups are enabled.' },
+    { name: 'available_images', type: 'select',
+      label: 'Base-OS-Image', options: {
+        data_source: {
+          name: 'hcloud_images',
+          arguments: { with_architecture: ['x86'] },
+        },
+        entity: {
+          name: 'description',
+        },
+        output_path_postfix: 'images',
+      } },
+  ]
+end
