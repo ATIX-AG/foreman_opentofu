2f5bedc7831003d79b35f6ad901166e790be01fb
Nadja Heitmann <nadjah@atix.de>
2026-02-16 14:49:01 +0000

    i18n - pulling from tx

---
+++ app/assets/javascripts/foreman_opentofu/locale/en/foreman_opentofu.js
@@ -0,0 +1,58 @@
+ locales['foreman_opentofu'] = locales['foreman_opentofu'] || {}; locales['foreman_opentofu']['en'] = {
+  "domain": "foreman_opentofu",
+  "locale_data": {
+    "foreman_opentofu": {
+      "": {
+        "Project-Id-Version": "foreman_opentofu 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2026-02-16 18:46+0000",
+        "Last-Translator": "FULL NAME <EMAIL@ADDRESS>",
+        "Language-Team": "LANGUAGE <LL@li.org>",
+        "Language": "",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Plural-Forms": "nplurals=INTEGER; plural=EXPRESSION;",
+        "lang": "en",
+        "domain": "foreman_opentofu",
+        "plural_forms": "nplurals=INTEGER; plural=EXPRESSION;"
+      },
+      "127.0.0.1": [
+        ""
+      ],
+      "Common fields": [
+        ""
+      ],
+      "OpenTofu Provider": [
+        ""
+      ],
+      "OpenTofu Script template": [
+        ""
+      ],
+      "OpenTofu Template": [
+        ""
+      ],
+      "Storage": [
+        ""
+      ],
+      "TODO: Description of ForemanPluginTemplate.": [
+        ""
+      ],
+      "URL": [
+        ""
+      ],
+      "Unable to find template specified by %s setting": [
+        ""
+      ],
+      "Unable to render provisioning template": [
+        ""
+      ],
+      "VM Config": [
+        ""
+      ],
+      "e.g. admin": [
+        ""
+      ]
+    }
+  }
+};
