cfb7b713d5472d9d535ebd95e2101b3693e2e7a9
Markus Bucher <bucher@atix.de>
2026-03-05 22:06:23 +0100

    Add more template-macros to format HCL

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -4,3 +4,9 @@ require 'test_helper'
 # Add plugin to FactoryBot's paths
 FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
 FactoryBot.reload
+
+def assert_snapshot(obj, name, expected)
+  path = ForemanOpentofu::Engine.root.join('test', 'fixtures', 'snapshots', obj.class.name.underscore, "#{name}.txt")
+
+  assert_equal File.read(path).strip, expected.strip
+end
