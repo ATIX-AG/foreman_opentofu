39bccf370eb6525cef4fe2c8579834043d1e167e
Markus Bucher <bucher@atix.de>
2026-05-28 13:08:58 +0000

    chore(i18n): pulling from tx

--- app/assets/javascripts/foreman_opentofu/locale/de_DE/foreman_opentofu.js
+++ app/assets/javascripts/foreman_opentofu/locale/de_DE/foreman_opentofu.js
@@ -3,7 +3,7 @@
   "locale_data": {
     "foreman_opentofu": {
       "": {
-        "Project-Id-Version": "foreman_opentofu 1.0.0",
+        "Project-Id-Version": "foreman_opentofu 0.0.4",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2026-02-16 11:45+0000",
         "Last-Translator": "Markus Bucher <bucher@atix.de>, 2026",
@@ -20,8 +20,59 @@
       "127.0.0.1": [
         "127.0.0.1"
       ],
+      "Add Interface": [
+        "Interface hinzufügen"
+      ],
+      "Add Volume": [
+        "Volume hinzufügen"
+      ],
+      "Cache slow calls to OpenTofu Compute resources to speed up page rendering": [
+        "Ergebnisse langsamer Abfragen zu OpenTofu zwischenspeichern um das Laden der Seiten zu beschleunigen"
+      ],
+      "Caching": [
+        "Zwischenspeicher"
+      ],
       "Common fields": [
         "Allgemeine Werte"
+      ],
+      "Disabled": [
+        "Deaktiviert"
+      ],
+      "Does this image support user data input (e.g. via cloud-init)?": [
+        "Unterstützt dieses Image die Eingabe von Benutzerdaten (z.B. via cloud-init)?"
+      ],
+      "Enable caching": [
+        "Caching aktivieren"
+      ],
+      "Enabled": [
+        "Aktiviert"
+      ],
+      "ID of the Image on the Hypervisor": [
+        "ID des Abbilds auf dem Hypervisor"
+      ],
+      "Image": [
+        "Abbild"
+      ],
+      "Image \\\"%{image}\\\" needs user data, but \\\"%{os}\\\" is not associated to any provisioning template of the kind user_data. Please associate it with a suitable template or uncheck 'User data' from the image definition.": [
+        "Abbild \\\"%{image}\\\" benötigt Benutzerdaten. Jedoch ist \\\"%{os}\\\" keine Bereitstellungsvorlage der Art user_data zugeordnet. Bitte weisen Sie eine geeignete Vorlage zu, oder deaktivieren sie \\\"Benutzerdaten\\\" in der Abbilddefinition."
+      ],
+      "Image ID": [
+        "Abbild ID"
+      ],
+      "Network interfaces": [
+        "Netzwerkinterface"
+      ],
+      "No networks found.": [
+        "Keine Netzwerke gefunden."
+      ],
+      "Number of Bits for the SSH-Key required to provision hosts on image-based providers.": [
+        "Bitanzahl des SSH-Schlüssels der für die abbildbasierte Bereitstellung von Hosts benötigt wird."
+      ],
+      "Number of seconds a run of OpenTofu command is allowed to report TfState back to the plugin.": [
+        "Anzahl der Sekunden in denen ein OpenTofu Befehl einen TfState an das Plugin zurücksenden darf."
+      ],
+      "OpenTofu": [
+        "OpenTofu"
       ],
       "OpenTofu Provider": [
         "OpenTofu Provider"
@@ -32,11 +83,26 @@
       "OpenTofu Template": [
         "OpenTofu Vorlage"
       ],
-      "Storage": [
-        "Speicher"
+      "Password to authenticate with - used for SSH finish step.": [
+        "Passwort mit dem sich authentifiziert wird - benötigt, um den letzten Schritt mit SSH abzuschließen."
+      ],
+      "Please select an image": [
+        "Bitte Abbild auswählen"
+      ],
+      "Power change operations are not enabled on this host.": [
+        "Power-Operationen werden auf diesem Host nicht unterstützt"
+      ],
+      "SSH-Key Length": [
+        "SSH-Schlüssellänge"
       ],
       "TODO: Description of ForemanPluginTemplate.": [
         ""
+      ],
+      "TfState Token Timeout": [
+        "TfState Token Timeout"
+      ],
+      "The user that is used to ssh into the instance, normally cloud-user, ec2-user, ubuntu, root etc": [
+        "Der Benutzer für die SSH-Verbindung zur Instanz. Normalerweise cloud-user, ec2-user, ubuntu, root, etc."
       ],
       "URL": [
         "URL"
@@ -50,8 +116,20 @@
       "VM Config": [
         "VM Konfiguration"
       ],
+      "add new network interface": [
+        "neues Netzwerkinterface hinzufügen"
+      ],
+      "add new storage volume": [
+        "neues Speichervolume hinzufügen"
+      ],
       "e.g. admin": [
         "z.B. admin"
+      ],
+      "remove network interface": [
+        "Netzwerkinterface entfernen"
+      ],
+      "remove storage volume": [
+        "entferne Speichervolume"
       ]
     }
   }
