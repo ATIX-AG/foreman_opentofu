6f3c04be9d6d02194676ff54c4dddad7900fe5e6
Markus Bucher <bucher@atix.de>
2026-05-28 14:00:29 +0200

    i18n - pulling from tx

---
+++ app/assets/javascripts/foreman_opentofu/locale/de_DE/foreman_opentofu.js
@@ -0,0 +1,58 @@
+ locales['foreman_opentofu'] = locales['foreman_opentofu'] || {}; locales['foreman_opentofu']['de_DE'] = {
+  "domain": "foreman_opentofu",
+  "locale_data": {
+    "foreman_opentofu": {
+      "": {
+        "Project-Id-Version": "foreman_opentofu 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2026-02-16 11:45+0000",
+        "Last-Translator": "Markus Bucher <bucher@atix.de>, 2026",
+        "Language-Team": "German (Germany) (https://app.transifex.com/foreman/teams/114/de_DE/)",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Language": "de_DE",
+        "Plural-Forms": "nplurals=2; plural=(n != 1);",
+        "lang": "de_DE",
+        "domain": "foreman_opentofu",
+        "plural_forms": "nplurals=2; plural=(n != 1);"
+      },
+      "127.0.0.1": [
+        "127.0.0.1"
+      ],
+      "Common fields": [
+        "Allgemeine Werte"
+      ],
+      "OpenTofu Provider": [
+        "OpenTofu Provider"
+      ],
+      "OpenTofu Script template": [
+        "OpenTofu Script Vorlage"
+      ],
+      "OpenTofu Template": [
+        "OpenTofu Vorlage"
+      ],
+      "Storage": [
+        "Speicher"
+      ],
+      "TODO: Description of ForemanPluginTemplate.": [
+        ""
+      ],
+      "URL": [
+        "URL"
+      ],
+      "Unable to find template specified by %s setting": [
+        "Konnte Vorlage mit der %s-Einstellung nicht finden"
+      ],
+      "Unable to render provisioning template": [
+        "Bereitstellungsvorlage konnte nicht verarbeitet werden"
+      ],
+      "VM Config": [
+        "VM Konfiguration"
+      ],
+      "e.g. admin": [
+        "z.B. admin"
+      ]
+    }
+  }
+};
--- app/assets/javascripts/foreman_opentofu/locale/en/foreman_opentofu.js
+++ app/assets/javascripts/foreman_opentofu/locale/en/foreman_opentofu.js
@@ -3,7 +3,7 @@
   "locale_data": {
     "foreman_opentofu": {
       "": {
-        "Project-Id-Version": "foreman_opentofu 1.0.0",
+        "Project-Id-Version": "foreman_opentofu 0.0.4",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2026-02-16 18:46+0000",
         "Last-Translator": "FULL NAME <EMAIL@ADDRESS>",
@@ -20,7 +20,55 @@
       "127.0.0.1": [
         ""
       ],
+      "Add Interface": [
+        ""
+      ],
+      "Add Volume": [
+        ""
+      ],
+      "Cache slow calls to Opentofu Compute resources to speed up page rendering": [
+        ""
+      ],
+      "Caching": [
+        ""
+      ],
       "Common fields": [
+        ""
+      ],
+      "Disabled": [
+        ""
+      ],
+      "Does this image support user data input (e.g. via cloud-init)?": [
+        ""
+      ],
+      "Enable caching": [
+        ""
+      ],
+      "Enabled": [
+        ""
+      ],
+      "ID of the Image on the Hypervisor": [
+        ""
+      ],
+      "Image": [
+        ""
+      ],
+      "Image \\\"%{image}\\\" needs user data, but \\\"%{os}\\\" is not associated to any provisioning template of the kind user_data. Please associate it with a suitable template or uncheck 'User data' from the image definition.": [
+        ""
+      ],
+      "Image ID": [
+        ""
+      ],
+      "Network interfaces": [
+        ""
+      ],
+      "No networks found.": [
+        ""
+      ],
+      "Number of Bits for the SSH-Key required to provision hosts on image-based providers.": [
+        ""
+      ],
+      "Number of seconds a run of Opentofu command is allowed to report tf-state back to the plugin.": [
         ""
       ],
       "OpenTofu Provider": [
@@ -32,10 +80,28 @@
       "OpenTofu Template": [
         ""
       ],
-      "Storage": [
+      "Opentofu": [
+        ""
+      ],
+      "Password to authenticate with - used for SSH finish step.": [
+        ""
+      ],
+      "Please select an image": [
+        ""
+      ],
+      "Power change operations are not enabled on this host.": [
+        ""
+      ],
+      "SSH-Key Length": [
         ""
       ],
       "TODO: Description of ForemanPluginTemplate.": [
+        ""
+      ],
+      "TfState Token Timeout": [
+        ""
+      ],
+      "The user that is used to ssh into the instance, normally cloud-user, ec2-user, ubuntu, root etc": [
         ""
       ],
       "URL": [
@@ -50,7 +116,19 @@
       "VM Config": [
         ""
       ],
+      "add new network interface": [
+        ""
+      ],
+      "add new storage volume": [
+        ""
+      ],
       "e.g. admin": [
+        ""
+      ],
+      "remove network interface": [
+        ""
+      ],
+      "remove storage volume": [
         ""
       ]
     }
