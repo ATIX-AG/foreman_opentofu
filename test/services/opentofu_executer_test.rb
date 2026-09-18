c3c6d0471c24f024eebea77c0f926fbf13ba3e84
Manisha Singhal <singhal@atix.de>
2026-09-18 10:46:26 +0200

    Remove Ovirt Support

--- test/services/opentofu_executer_test.rb
+++ test/services/opentofu_executer_test.rb
@@ -206,22 +206,6 @@ module ForemanOpentofu
       end
     end
 
-    test '#run passes all connection variables for oVirt' do
-      compute_resource = FactoryBot.build_stubbed(:opentofu_ovirt_cr)
-      executor = OpentofuExecuter.new(compute_resource)
-      executor.stubs(:render_template).returns('rendered template')
-      expected_variables = {
-        username: compute_resource.user,
-        password: compute_resource.password,
-        endpoint: compute_resource.url,
-      }
-      AppWrapper.expects(:new).with(kind_of(String), variables: expected_variables).returns(@app_mock)
-
-      stub_opentofu_tmp_dir do
-        executor.run_test_connection
-      end
-    end
-
     test '#run passes only the password variable for Hetzner' do
       compute_resource = FactoryBot.build_stubbed(:opentofu_hetzner_cr)
       executor = OpentofuExecuter.new(compute_resource)
